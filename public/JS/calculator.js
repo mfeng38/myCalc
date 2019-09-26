04d3ae1808df4d010427f034b18f8e78ff096dca
Malik F <45907360+mfeng38@users.noreply.github.com>
2019-09-25 19:25:15 -0700

    almost done, 1 feature left

--- public/JS/calculator.js
+++ public/JS/calculator.js
@@ -1,30 +1,39 @@
 var m = document.getElementById('mean');
 var w = document.getElementById('weighted');
+var rowButton = document.getElementById('rowbttn');
 var gr = document.getElementsByClassName('inpbox');
+var tbl = document.getElementById('tbl1');
+var numRows = 4;
+
+var numerator = document.getElementsByName('grade');
+var denominator = document.getElementsByName('total');
+var weights = document.getElementsByName('weight');
+var result = document.getElementById('answr');
+var prcnt = document.getElementsByName('percent');
+
 for(i=0; i < gr.length; i++)
 {
   gr[i].oninput = percent;
 }
 m.onclick = mean;
 w.onclick = weighted;
+rowButton.onclick = addRow;
 
 function percent(){
-  var numerator = document.getElementsByName('grade');
-  var denominator = document.getElementsByName('total');
-  var rslt = document.getElementsByName('percent');
   for(i=0; i < numerator.length; i++)
   {
     if(isFinite(numerator[i].value/denominator[i].value))
     {
-      rslt[i].innerText = parseFloat( ( numerator[i].value/denominator[i].value ) * 100 ).toFixed(2) + "%";
+      prcnt[i].innerHTML = parseFloat( ( numerator[i].value/denominator[i].value ) * 100 ).toFixed(2) + "%";
+    }
+    else
+    {
+      prcnt[i].innerHTML = "";
     }
   }
 }
 
 function mean(){
-  var numerator = document.getElementsByName('grade');
-  var denominator = document.getElementsByName('total');
-  var result = document.getElementById('answr');
   var sum = 0;
   var counter = 0;
   for(i=0; i < numerator.length; i++)
@@ -42,10 +51,6 @@ function mean(){
 }
 
 function weighted(){
-  var numerator = document.getElementsByName('grade');
-  var denominator = document.getElementsByName('total');
-  var weights = document.getElementsByName('weight');
-  var result = document.getElementById('answr');
   var sum = 0;
   var counter = 0;
   for(i=0; i < numerator.length; i++)
@@ -59,5 +64,24 @@ function weighted(){
   if(isFinite(sum/counter))
   {
     result.innerHTML = parseFloat( (sum/counter)*100 ).toFixed(2) + "%";
+  }
+}
+
+function addRow(){
+  var row = tbl.insertRow();
+  numRows++;
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  var cell3 = row.insertCell(2);
+  var cell4 = row.insertCell(3);
+  var cell5 = row.insertCell(4);
+  cell1.innerHTML = "Activity " + numRows;
+  cell2.innerHTML = "A" + numRows;
+  cell3.innerHTML += "<input type='text' name='weight' class='inpbox'>";
+  cell4.innerHTML += "<input type='text' name='grade' class='inpbox'> <font size='5'>/</font><br></br><input type='text' name='total' class='inpbox'>";
+  cell5.setAttribute("name", "percent");
+  for(i=gr.length-3; i < gr.length; i++)
+  {
+    gr[i].oninput = percent;
   }
 }
