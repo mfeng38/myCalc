48b7097decef2f1986bf9523645b813908275178
Malik F <45907360+mfeng38@users.noreply.github.com>
2019-10-14 12:09:54 -0700

    db

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
-const cool = require('cool-ascii-faces')
-const express = require('express')
-const path = require('path')
+const express = require('express');
+const path = require('path');
 const PORT = process.env.PORT || 5000
+var app = express();
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.send(cool()))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const { Pool } = require('pg');
+var pool;
+pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+   ssl: true
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => res.render('pages/index'));
+app.get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect()
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+app.get('/cool', (req, res) => res.send(cool()));
+app.get('/tokimon', (req,res) => { res.render('pages/tokimon')});
+app.post('/inputs', (req, res) => {
+  console.log('test');
+  var name = req.body.nam;
+  var val1 = req.body.fly;
+  res.send(`Hello, ${name}. \n ${val1}`);
+});
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
